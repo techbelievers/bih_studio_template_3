62c6eac383571287993c422c7ff799b46f0c5428
raj-umakant-patil <techbelievers@gmail.com>
2024-09-28 16:34:18 +0530

    domain

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,13 +1,16 @@
 // middleware.js
 import { NextResponse } from 'next/server';
-import config from './config';
 
 export function middleware(req) {
   const host = req.headers.get('host');
   const finalDomain = host === 'localhost:3000' ? 'buyindiahomes.in' : host;
 
-  // Add the website domain to the request headers or context
-  req.headers.set('X-Website-Domain', finalDomain);
+  // Add the website domain to the request headers
+  req.headers.set('x-website-domain', finalDomain);
 
-  return NextResponse.next();
+  return NextResponse.next({
+    request: {
+      headers: req.headers,
+    },
+  });
 }
