34f3859e85d41e16387b21ad317e34b2266b32df
raj-umakant-patil <techbelievers@gmail.com>
2024-09-28 11:32:22 +0530

    domain

--- Config.js
+++ Config.js
@@ -2,7 +2,15 @@ const BASE_URL = 'https://buyindiahomes.in/api';
 // const WEBSITE_DOMAIN = "buyindiahomes.in";
 const WEBSITE_DOMAIN = typeof window !== 'undefined' ? window.location.hostname : 'buyindiahomes.in';
 console.log('Website Domain:', WEBSITE_DOMAIN);
-const getApiUrl = (endpoint) => `${BASE_URL}/${endpoint}?website=${WEBSITE_DOMAIN}`;
+// const getApiUrl = (endpoint) => `${BASE_URL}/${endpoint}?website=${WEBSITE_DOMAIN}`;
+const getApiUrl = (endpoint) => {
+  // Log the endpoint and the website domain
+  console.log('API Endpoint:', endpoint);
+  console.log('Website Domain:', WEBSITE_DOMAIN);
+  console.log(`${BASE_URL}/${endpoint}?website=${WEBSITE_DOMAIN}`);
+  return `${BASE_URL}/${endpoint}?website=${WEBSITE_DOMAIN}`;
+};
+
 console.log('Website Domain:', `${BASE_URL}/header?website=${WEBSITE_DOMAIN}`);
 
 const API = {
