c6515087eb54b72f4ed6d312b58a23e3f8de9af1
raj-umakant-patil <techbelievers@gmail.com>
2024-11-29 18:24:16 +0530

    middler

--- middleware.js
+++ middleware.js
@@ -2,22 +2,27 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-  const host = req.headers.get('x-forwarded-host') || 'localhost:3000'; 
-  // const finalDomain = host === 'localhost:3000' ? 'builderkonnect.com' : host;
-  const finalDomain =  host;
+  // const host = req.headers.get('x-forwarded-host') || 'localhost:3000'; 
+  // const finalDomain =  host;
+  // console.log('middleware : ',finalDomain);
+  // req.headers.set('x-website-domain', finalDomain);
+  // return NextResponse.next({
+  //   request: {
+  //     headers: req.headers,
+  //   },
+  // });
 
-  console.log('middleware : ',finalDomain);
-  // Add the website domain to the request headers
-  req.headers.set('x-website-domain', finalDomain);
-
-  // console.log('middleware');
-  // console.log(req.headers);
-
-  return NextResponse.next({
-    request: {
-      headers: req.headers,
-    },
-  });
+  const hostname = req.headers.get('host');
+  const protocol = req.headers.get('x-forwarded-proto') || 'http';
+  // Redirect non-www to www
+  if (!hostname.startsWith('www.') && !hostname.startsWith('localhost')) {
+    return NextResponse.redirect(`https://www.${hostname}${req.nextUrl.pathname}`, 301);
+  }
+  // Redirect HTTP to HTTPS
+  if (protocol !== 'https') {
+    return NextResponse.redirect(`https://${hostname}${req.nextUrl.pathname}`, 301);
+  }
+  return NextResponse.next();
 }
 
 // Specify paths for which the middleware should apply
