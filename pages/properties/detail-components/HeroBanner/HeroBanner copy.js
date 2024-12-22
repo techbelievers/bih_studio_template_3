91b78fa83c59ccdeb081f3af6568f2058131f9e0
raj-umakant-patil <techbelievers@gmail.com>
2024-12-22 11:05:35 +0530

    fix

--- pages/properties/detail-components/HeroBanner/HeroBanner copy.js
+++ pages/properties/detail-components/HeroBanner/HeroBanner copy.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { API } from "../../../../../Config";
+import { API } from "../../../../Config";
 import styles from "./HeroBanner.module.css";
 
 const GalleryWithEnquiry = ({ propertyDetails, slug }) => {
